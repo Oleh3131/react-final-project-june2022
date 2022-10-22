c05ea39cdc724bb9b0fd85345f891daee7c4bb4f
Oleh3131 <oshevskyioleh@gmail.com>
2022-10-22 23:51:45 +0300

    add info movies+genres+moviesById

--- src/components/GenreDetailsInfo/GenreDetailsInfo.js
+++ src/components/GenreDetailsInfo/GenreDetailsInfo.js
@@ -1,14 +1,19 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {useLocation} from "react-router-dom";
 
 import css from './GenreDetailsInfo.module.css'
+import Reviews from "../Reviews/Reviews";
+import {axiosService} from "../../services";
 
 
 const GenreDetailsInfo = () => {
 
     const {state} = useLocation();
 
+    const [reviews, setReviews] = useState([]);
+
     const {
+        id,
         original_title,
         poster_path,
         overview,
@@ -20,24 +25,40 @@ const GenreDetailsInfo = () => {
     } = state.movie;
 
 
+
+    useEffect(() => {
+
+        axiosService.get(`/movie/${id}/reviews`).then(({data}) => setReviews(data.results))
+
+    }, [id])
+
+
     return (
-        <div className={css.CardBlock}>
+        <div className={css.MainCard}>
+            <div className={css.CardBlock}>
 
-            <div className={css.ImgBlock}>
-                <img src={`https://image.tmdb.org/t/p/w200${poster_path}`} alt={original_title}/>
+                <div className={css.ImgBlock}>
+                    <img src={`https://image.tmdb.org/t/p/w200${poster_path}`} alt={original_title}/>
+                </div>
+
+                <div className={css.TextBlock}>
+                    <h1>{original_title}</h1>
+                    <p>Original language: {original_language}</p>
+                    <p>Overview: {overview}</p>
+                    <p>Popularity: {popularity}</p>
+                    <p>Release date: {release_date}</p>
+                    <p>Vote average: {vote_average}</p>
+                    <p>Vote count: {vote_count}</p>
+                </div>
+
             </div>
 
-            <div className={css.TextBlock}>
-                <h1>{original_title}</h1>
-                <p>Original language: {original_language}</p>
-                <p>Overview: {overview}</p>
-                <p>Popularity: {popularity}</p>
-                <p>Release date: {release_date}</p>
-                <p>Vote average: {vote_average}</p>
-                <p>Vote count: {vote_count}</p>
-            </div>
+            <h2>Comments:</h2>
+
+            {reviews.map(review => <Reviews key={review.id} review={review}/>)}
 
         </div>
+
     );
 };
 
