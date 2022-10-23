b360698a4c7c9194f054c29e93c7a21729e53b93
Oleh3131 <oshevskyioleh@gmail.com>
2022-10-24 02:16:12 +0300

    add info movies+genres+moviesById

--- src/components/NotFoundPage/NotFoundPage.js
+++ src/components/NotFoundPage/NotFoundPage.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import css from './NotFoundPage.module.css'
+import img from '../../images/error-404-06.jpg'
+
 const NotFoundPage = () => {
     return (
-        <div>
-            NotFoundPage
+        <div className={css.NotFoundBlock}>
+            <img src={img} alt="Not Found Page"/>
         </div>
     );
 };
